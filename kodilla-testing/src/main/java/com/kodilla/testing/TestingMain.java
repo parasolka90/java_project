bb59d8ca41b3c307e87629725e9fe490a3de30dd
parasolka90 <parasolka90@gmail.com>
2017-08-15 12:16:20 +0200

    calculator testing

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,25 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
-    public static void main(String[] args){
-        System.out.println("Moduł 6.0. Wprowadzenie to testowania oprogramowania");
+    public static void main(String[] args) {
+        Calculator calculate = new Calculator();
+        int addingResult =calculate.add(2,2);
+        int subtractingResult =calculate.substract(2,2);
+
+
+        if(addingResult == 4){
+            System.out.print("adding test ok");
+        } else {
+            System.out.println("adding test - error!");
+        }
+        System.out.println("");
+
+        if(subtractingResult == 0){
+            System.out.println("subtracting - test ok");
+        } else {
+            System.out.println("subtracting  test - error!");
+        }
     }
 }
