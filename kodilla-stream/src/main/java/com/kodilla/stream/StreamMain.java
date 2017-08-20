e946973213f69bbf9e34acef7fa5f75a93ccc71d
parasolka90 <parasolka90@gmail.com>
2017-08-20 16:58:25 +0200

    7.2 ForumUser

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -2,15 +2,10 @@ package com.kodilla.stream;
 
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 
 public class StreamMain{
     public static void main(String[]args){
-        PoemBeautifier poemBeautifier = new PoemBeautifier();
-        String b="bardzo";
-
-        poemBeautifier.beautify(b,(a -> "Lubie pizze "+a+" i burgerki tez lubie"));
-        poemBeautifier.beautify(b,(a ->a.toUpperCase()));
-        poemBeautifier.beautify(b,(a->a.replace('r','x')));
-        poemBeautifier.beautify(b,(a->b.substring(3)));
+        NumbersGenerator.generateEven(20);
     }
 }
