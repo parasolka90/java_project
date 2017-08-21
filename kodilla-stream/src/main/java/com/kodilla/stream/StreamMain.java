c62a9708f04fe07b2495565d21c8a602bfccce99
parasolka90 <parasolka90@gmail.com>
2017-08-21 18:30:06 +0200

    7.3 ForumUser

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,11 +1,29 @@
 package com.kodilla.stream;
 
+import com.kodilla.stream.immutable.Forum;
+import com.kodilla.stream.immutable.ForumUser;
 
-import com.kodilla.stream.beautifier.PoemBeautifier;
-import com.kodilla.stream.iterate.NumbersGenerator;
+import java.time.LocalDate;
+import java.util.Map;
+import java.util.stream.Collectors;
 
-public class StreamMain{
-    public static void main(String[]args){
-        NumbersGenerator.generateEven(20);
+public class StreamMain {
+    public static void main(String[]args) {
+        Forum forum = new Forum();
+        LocalDate a =LocalDate.now().minusYears(20);
+
+
+        Map<Integer,ForumUser> resultMapForumUser = forum.getUserList().stream()
+                .filter(user->user.getSex()=='M')
+                .filter(user->user.getBirthdate().isBefore(a))
+                .filter(user->user.getPostCount()>0)
+
+                .collect(Collectors.toMap(ForumUser::getId, user->user));
+
+
+        resultMapForumUser.entrySet().stream()
+                .forEach(System.out::println);
+
+
     }
 }
--- kodilla-stream/src/main/java/com/kodilla/stream/immutable/ForumUser.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/immutable/ForumUser.java
@@ -1,19 +1,50 @@
 package com.kodilla.stream.immutable;
 
-public final class ForumUser {
-    private final String username;
-    private final String realName;
+import java.time.LocalDate;
 
-    public ForumUser(final String username,final String realName) {
+public final class ForumUser {
+    final int id;
+    final String username;
+    final char sex;
+    final LocalDate birthdate;
+    final int postCount;
+
+    public ForumUser(final int id, final String username, final char sex,
+                     final int yearOfBirth, final int monthOfBirth,
+                     final int dayOfBirth, final int postCount) {
+        this.id = id;
         this.username = username;
-        this.realName = realName;
+        this.sex = sex;
+        this.birthdate = LocalDate.of(yearOfBirth, monthOfBirth, dayOfBirth);
+        this.postCount = postCount;
+
+    }
+
+    public int getId() {
+        return id;
     }
 
     public String getUsername() {
         return username;
     }
 
-    public String getRealName() {
-        return realName;
+    public char getSex() {
+        return sex;
+    }
+
+    public LocalDate getBirthdate() {
+        return birthdate;
+    }
+
+    public int getPostCount() {
+        return postCount;
+    }
+
+
+    @Override
+    public String toString() {
+        return "ForumUser [id=" + id + ", username=" + username + ", sex=" + sex + ", birthdate=" + birthdate
+                + ", postCount=" + postCount +"]";
     }
 }
+
