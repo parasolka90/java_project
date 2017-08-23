b1c39c2f027efcc8eb79217bca216cddc7513785
parasolka90 <parasolka90@gmail.com>
2017-08-23 18:41:52 +0200

    8.3 ExceptionHandling

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
@@ -0,0 +1,14 @@
+package com.kodilla.exception.test;
+
+public class ExceptionHandling {
+    public static void main(String[]args){
+        SecondChallenge challenge = new SecondChallenge();
+        try{
+            challenge.probablyIWillThrowException(1,6);
+        } catch(Exception e){
+            System.out.println("Error: "+e);
+        } finally {
+            System.out.print("finally");
+        }
+    }
+}
