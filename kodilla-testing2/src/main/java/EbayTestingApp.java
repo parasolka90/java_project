6db618e85ea24f7dc3ce1d2514f21b080a73e03c
parasolka90 <parasolka90@gmail.com>
2017-12-11 21:14:17 +0100

    selenium tests ebay

--- kodilla-testing2/src/main/java/EbayTestingApp.java
+++ kodilla-testing2/src/main/java/EbayTestingApp.java
@@ -4,12 +4,11 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 public class EbayTestingApp {
-    public static final String EBAY_URL = "https://www.ebay.com";
     public static final String SEARCHFIELD = "gh-ac";
 
     public static void main(String[] args) {
         WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.CHROME);
-        driver.get(EBAY_URL);
+        driver.get("https://www.ebay.com");
 
         WebElement searchField = driver.findElement(By.id(SEARCHFIELD));
         searchField.sendKeys("Laptop");
